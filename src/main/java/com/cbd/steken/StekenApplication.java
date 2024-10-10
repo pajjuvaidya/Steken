ea21987635b10aa98a152408b4738b3ca8c8c9bf
AkashSky09 <85094440+AkashSky09@users.noreply.github.com>
2024-10-10 22:28:59 +0530

    Spring initialised project

---
+++ src/main/java/com/cbd/steken/StekenApplication.java
@@ -0,0 +1,13 @@
+package com.cbd.steken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StekenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StekenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cbd/steken/StekenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbd.steken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StekenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
