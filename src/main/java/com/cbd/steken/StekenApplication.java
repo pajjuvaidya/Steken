9a1ed83ca4f0c01cff39e0ee42a8fcac3aa49419
pajjuvaidya <39592270+pajjuvaidya@users.noreply.github.com>
2024-10-13 13:31:14 +0530

    Update StekenApplication.java

--- src/main/java/com/cbd/steken/StekenApplication.java
+++ src/main/java/com/cbd/steken/StekenApplication.java
@@ -2,8 +2,9 @@ package com.cbd.steken;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class StekenApplication {
 
     public static void main(String[] args) {
